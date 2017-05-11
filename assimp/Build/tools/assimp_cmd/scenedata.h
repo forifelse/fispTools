d735123966996442ca9a7193860fbd3fcab7297a
forifelse <forifelse@163.coom>
2017-05-11 17:45:21 +0800

    add operator <()

--- assimp/Build/tools/assimp_cmd/scenedata.h
+++ assimp/Build/tools/assimp_cmd/scenedata.h
@@ -7,6 +7,21 @@ struct SDCamera
 	float	rx,ry,rz;
 
 	SDCamera() { memset(this, 0, sizeof(SDCamera)); }
+	SDCamera(const SDCamera& o)
+	{
+		tx = o.tx; ty = o.ty; tz = o.tz;
+		rx = o.rx; ry = o.ry; rz = o.rz;
+	}
+	SDCamera& operator=(const SDCamera& o)
+	{
+		tx = o.tx; ty = o.ty; tz = o.tz;
+		rx = o.rx; ry = o.ry; rz = o.rz;
+		return *this;
+	}
+	bool operator<(const SDCamera& o)
+	{
+		return true;
+	}
 };
 
 struct SDLitPoint
@@ -18,6 +33,27 @@ struct SDLitPoint
 	float	intensity;
 
 	SDLitPoint() { memset(this, 0, sizeof(SDLitPoint)); }
+	SDLitPoint(const SDLitPoint& o)
+	{
+		strName = o.strName;
+		x = o.x; y = o.y; z = o.z;
+		r = o.r; g = o.g; b = o.b;
+		radius = o.radius;
+		intensity = o.intensity;
+	}
+	SDLitPoint& operator=(const SDLitPoint& o)
+	{
+		strName = o.strName;
+		x = o.x; y = o.y; z = o.z;
+		r = o.r; g = o.g; b = o.b;
+		radius = o.radius;
+		intensity = o.intensity;
+		return *this;
+	}
+	bool operator<(const SDLitPoint& o)
+	{
+		return true;
+	}
 };
 
 struct SDLitSpot : public SDLitPoint
@@ -26,6 +62,25 @@ struct SDLitSpot : public SDLitPoint
 	float	inner, outer;
 
 	SDLitSpot() { memset(this, 0, sizeof(SDLitSpot)); }
+	SDLitSpot(const SDLitSpot& o)
+	{
+		__super::operator=(o);
+		toward = o.toward;
+		inner = o.inner;
+		outer = o.outer;
+	}
+	SDLitSpot& operator=(const SDLitSpot& o)
+	{
+		__super::operator=(o);
+		toward = o.toward;
+		inner = o.inner;
+		outer = o.outer;
+		return *this;
+	}
+	bool operator<(const SDLitSpot& o)
+	{
+		return true;
+	}
 };
 
 struct SDLitDire
@@ -42,13 +97,48 @@ struct SDMaterial
 	float	ambientR, ambientG, ambientB;
 	float	diffuseR, diffuseG, diffuseB;
 	float	specularR, specularG, specularB;
-	float	emmissiveR, emmissiveG, emmissiveB;
+	float	emissiveR, emissiveG, emissiveB;
 	float	metallic;
 	float	roughness;
 	float	shinness;
 	bool	bTwoSided;
 
 	SDMaterial() { memset(this, 0, sizeof(SDMaterial)); }
+	SDMaterial(const SDMaterial& o)
+	{
+		strName = o.strName;
+		strColorTexture = o.strColorTexture;
+		strNormalMap = o.strNormalMap;
+		strSpecularMap = o.strSpecularMap;
+		ambientR = o.ambientR; ambientG = o.ambientG; ambientB = o.ambientB;
+		diffuseR = o.diffuseR; diffuseG = o.diffuseG; diffuseB = o.diffuseB;
+		specularR = o.specularR; specularG = o.specularG; specularB = o.specularB;
+		emissiveR = o.emissiveR; emissiveG = o.emissiveG; emissiveB = o.emissiveB;
+		metallic = o.metallic;
+		roughness = o.roughness;
+		shinness = o.shinness;
+		bTwoSided = o.bTwoSided;
+	}
+	SDMaterial& operator=(const SDMaterial& o)
+	{
+		strName = o.strName;
+		strColorTexture = o.strColorTexture;
+		strNormalMap = o.strNormalMap;
+		strSpecularMap = o.strSpecularMap;
+		ambientR = o.ambientR; ambientG = o.ambientG; ambientB = o.ambientB;
+		diffuseR = o.diffuseR; diffuseG = o.diffuseG; diffuseB = o.diffuseB;
+		specularR = o.specularR; specularG = o.specularG; specularB = o.specularB;
+		emissiveR = o.emissiveR; emissiveG = o.emissiveG; emissiveB = o.emissiveB;
+		metallic = o.metallic;
+		roughness = o.roughness;
+		shinness = o.shinness;
+		bTwoSided = o.bTwoSided;
+		return *this;
+	}
+	bool operator<(const SDMaterial& o)
+	{
+		return true;
+	}
 };
 
 struct SDTexture
@@ -72,16 +162,73 @@ struct SDGeometry
 	float*			pVerClr;
 
 	SDGeometry() { memset(this, 0, sizeof(SDGeometry)); }
+	SDGeometry(const SDGeometry& o)
+	{
+		uNumVertex = o.uNumVertex;
+		uNumFace = o.uNumFace;
+		uNumUV = o.uNumUV;
+		uNumNor = o.uNumNor;
+		uNumTang = o.uNumTang;
+		uNumVerClr = o.uNumVerClr;
+		// ?
+		pVertices = o.pVertices;
+		pTriangles = o.pTriangles;
+		pUV = o.pUV;
+		pNormal = o.pNormal;
+		pTangent = o.pTangent;
+		pVerClr = o.pVerClr;
+
+	}
+	SDGeometry& operator=(const SDGeometry& o)
+	{
+		uNumVertex = o.uNumVertex;
+		uNumFace = o.uNumFace;
+		uNumUV = o.uNumUV;
+		uNumNor = o.uNumNor;
+		uNumTang = o.uNumTang;
+		uNumVerClr = o.uNumVerClr;
+		// ?
+		pVertices = o.pVertices;
+		pTriangles = o.pTriangles;
+		pUV = o.pUV;
+		pNormal = o.pNormal;
+		pTangent = o.pTangent;
+		pVerClr = o.pVerClr;
+		return *this;
+	}
+	bool operator<(const SDGeometry& o)
+	{
+		return true;
+	}
 };
 
 struct SDSubMesh
 {
 	unsigned int	uGeomIdx;
 	unsigned int	uMateIdx;
-	unsigned int	uSubIndex;
+	unsigned int	uSubIdx;
 	bool			bCollision;
 
 	SDSubMesh() { memset(this, 0, sizeof(SDSubMesh)); }
+	SDSubMesh(const SDSubMesh& o)
+	{
+		uGeomIdx = o.uGeomIdx;
+		uMateIdx = o.uMateIdx;
+		uSubIdx = o.uSubIdx;
+		bCollision = o.bCollision;
+	}
+	SDSubMesh& operator=(const SDSubMesh& o)
+	{
+		uGeomIdx = o.uGeomIdx;
+		uMateIdx = o.uMateIdx;
+		uSubIdx = o.uSubIdx;
+		bCollision = o.bCollision;
+		return *this;
+	}
+	bool operator<(const SDSubMesh& o)
+	{
+		return true;
+	}
 };
 
 struct SDMesh
@@ -106,6 +253,19 @@ struct SDMesh
 	// instance ?
 
 	SDMesh() { memset(this, 0, sizeof(SDMesh)); }
+	SDMesh(const SDMesh& o)
+	{
+		memcpy(this, &o, sizeof(SDMesh));
+	}
+	SDMesh& operator=(const SDMesh& o)
+	{
+		memcpy(this, &o, sizeof(SDMesh));
+		return *this;
+	}
+	bool operator<(const SDMesh& o)
+	{
+		return true;
+	}
 };
 
 struct SDRoot
@@ -114,6 +274,21 @@ struct SDRoot
 	unsigned int*	pChildren;
 
 	SDRoot() { memset(this, 0, sizeof(SDRoot)); }
+	SDRoot(const SDRoot& o)
+	{
+		uNumChild = o.uNumChild;
+		pChildren = o.pChildren;
+	}
+	SDRoot& operator=(const SDRoot& o)
+	{
+		uNumChild = o.uNumChild;
+		pChildren = o.pChildren;
+		return *this;
+	}
+	bool operator<(const SDRoot& o)
+	{
+		return true;
+	}
 };
 
 struct SDBlob
@@ -127,6 +302,19 @@ struct SDBlob
 	SDLitDire*		pLitDire;
 
 	SDBlob() { memset(this, 0, sizeof(SDBlob)); }
+	SDBlob(const SDBlob& o)
+	{
+		memcpy(this, &o, sizeof(SDBlob));
+	}
+	SDBlob& operator=(const SDBlob& o)
+	{
+		memcpy(this, &o, sizeof(SDBlob));
+		return *this;
+	}
+	bool operator<(const SDBlob& o)
+	{
+		return true;
+	}
 };
 
 struct SDScene
@@ -145,6 +333,19 @@ struct SDScene
 	SDRoot*			pRoot;
 
 	SDScene() { memset(this, 0, sizeof(SDScene)); }
+	SDScene(const SDScene& o)
+	{
+		memcpy(this, &o, sizeof(SDScene));
+	}
+	SDScene& operator=(const SDScene& o)
+	{
+		memcpy(this, &o, sizeof(SDScene));
+		return *this;
+	}
+	bool operator<(const SDScene& o)
+	{
+		return true;
+	}
 };
 
 struct SceneRW
